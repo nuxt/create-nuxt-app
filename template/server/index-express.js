bba2cdfdfbe6dd7e05f1a5924ea7eba65d588198
Clark Du <clark.duxin@gmail.com>
2018-01-10 20:17:08 +0800

    feat: upgrade nuxt to 1.0.0

--- template/server/index-express.js
+++ template/server/index-express.js
@@ -13,18 +13,21 @@ app.set('port', port)
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
-// Init Nuxt.js
-const nuxt = new Nuxt(config)
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
 
-// Build only in dev mode
-if (config.dev) {
-  const builder = new Builder(nuxt)
-  builder.build()
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
-
-// Give nuxt middleware to express
-app.use(nuxt.render)
-
-// Listen the server
-app.listen(port, host)
-console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+start()
