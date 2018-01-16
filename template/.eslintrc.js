9033fd71298981a047dcb81a0ec0d8036f2a7c97
Sébastien Chopin <seb@chopin.io>
2018-01-16 14:48:17 +0100

    Update .eslintrc.js

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
-  rules: {
-    'space-before-function-paren': [
-      2,
-      {
-        anonymous: 'always',
-        named: 'never'
-      }
-    ],
-  },
-  globals: {<% if (server === 'adonis') { %>
-    use: true,
-    make: true<% } %>
-  }
+  rules: {}
 }
