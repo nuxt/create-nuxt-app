4b71ee457cf273e985c9ab7293d1e69907b3f6f0
Mark Pinero <markspinero@gmail.com>
2018-09-22 15:30:56 -0700

    improvement: add sample at-rule styles

--- template/nuxt/pages/index.vue
+++ template/nuxt/pages/index.vue
@@ -33,6 +33,14 @@ export default {
 </script>
 
 <style>
+<% if (ui === 'tailwind') { %>
+/* Sample `apply` at-rules with Tailwind CSS
+.container
+{
+  @apply min-h-screen flex justify-center items-center text-center;
+}
+*/
+<% } %>
 .container
 {
   min-height: 100vh;
