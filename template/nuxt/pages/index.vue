a188316125a353f65d6501c5912ac52ce2ba530a
Liam Potter <me@liam-potter.co.uk>
2018-09-25 12:19:54 +0100

    fix default linting errors when enabling prettier option

--- template/nuxt/pages/index.vue
+++ template/nuxt/pages/index.vue
@@ -41,33 +41,33 @@ export default {
 }
 */
 <% } %>
-.container
-{
+.container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
-.subtitle
-{
+
+.subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-.links
-{
+
+.links {
   padding-top: 15px;
 }
 </style>
