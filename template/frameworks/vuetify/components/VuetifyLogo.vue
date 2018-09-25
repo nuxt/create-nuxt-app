00289033408ea1abf21774091586ca6989bb3954
Liam Potter <me@liam-potter.co.uk>
2018-09-25 12:57:25 +0100

    fix centering issue for tailwind option, fix vuetify styles for prettier option

--- template/frameworks/vuetify/components/VuetifyLogo.vue
+++ template/frameworks/vuetify/components/VuetifyLogo.vue
@@ -6,14 +6,15 @@
 </template>
 
 <style>
-  .VuetifyLogo {
-    width: 180px;
-    transform: rotateY(560deg);
-    animation: turn 3.5s ease-out forwards 1s;
+.VuetifyLogo {
+  width: 180px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
   }
-  @keyframes turn {
-    100% {
-      transform: rotateY(0deg);
-    }
-  }
+}
 </style>
--- template/nuxt/pages/index.vue
+++ template/nuxt/pages/index.vue
@@ -35,11 +35,11 @@ export default {
 <style>
 <% if (ui === 'tailwind') { %>
 /* Sample `apply` at-rules with Tailwind CSS
-.container
-{
-  @apply min-h-screen flex justify-center items-center text-center;
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+
 <% } %>
 .container {
   min-height: 100vh;
