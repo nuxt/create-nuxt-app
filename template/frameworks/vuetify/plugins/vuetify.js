0e93df4594050b4e284a09484277090f4cbf8caf
SHARON <sharushj690@gmail.com>
2018-10-14 20:05:12 +0530

    Updated with Vuetify original material colors

--- template/frameworks/vuetify/plugins/vuetify.js
+++ template/frameworks/vuetify/plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    primary: colors.blue.darken2,
+    accent: colors.blue.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2
   }
 })
