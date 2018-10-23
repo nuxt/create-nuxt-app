7dd7c345aa1f378a1174d79b51207e37922af05c
Uni Sayo <unibtc@gmail.com>
2018-10-24 02:14:07 +0800

    Simplify Koa Middleware (#124)
    Simply migrating this PR https://github.com/nuxt-community/koa-template/pull/39 here.

--- template/server/index-koa.js
+++ template/server/index-koa.js
@@ -21,16 +21,10 @@ async function start() {
   }
 
   app.use(ctx => {
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
-      })
-    })
+    ctx.status = 200
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
   })
 
   app.listen(port, host)
