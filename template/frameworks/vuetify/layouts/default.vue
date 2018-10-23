209b14871d24f2e1fd228be964690b799d2dfe56
SHARON <sharushj690@gmail.com>
2018-10-23 14:32:36 +0530

    Update default.vue

--- template/frameworks/vuetify/layouts/default.vue
+++ template/frameworks/vuetify/layouts/default.vue
@@ -1,40 +1,39 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
+      :mini-variant.sync="miniVariant"
       :clipped="clipped"
+      v-model="drawer"
       fixed
       app
     >
       <v-list>
         <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
           router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
           exact
         >
           <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
+      fixed app
       :clipped-left="clipped"
-      fixed
-      app
     >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
       <v-btn
         icon
@@ -48,7 +47,8 @@
       >
         <v-icon>remove</v-icon>
       </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
       <v-btn
         icon
         @click.stop="rightDrawer = !rightDrawer"
@@ -62,9 +62,9 @@
       </v-container>
     </v-content>
     <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
       temporary
+      :right="right"
+      v-model="rightDrawer"
       fixed
     >
       <v-list>
@@ -80,29 +80,21 @@
       :fixed="fixed"
       app
     >
-      <span>&copy; 2017</span>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       clipped: false,
       drawer: true,
       fixed: false,
       items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
       ],
       miniVariant: false,
       right: true,
