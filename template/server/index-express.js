a235d79ebc081af2e903bbd9d4d4d2406b4677fe
Sibbngheid <sibbngheid@gmail.com>
2018-10-28 18:29:55 +0300

    update express

--- template/server/index-express.js
+++ template/server/index-express.js
@@ -2,10 +2,6 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
-
-app.set('port', port)
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
@@ -14,6 +10,9 @@ config.dev = !(process.env.NODE_ENV === 'production')
 async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
+
+  const host = nuxt.options.server.host || process.env.HOST || '127.0.0.1'
+  const port = nuxt.options.server.port || process.env.PORT || 3000
 
   // Build only in dev mode
   if (config.dev) {
