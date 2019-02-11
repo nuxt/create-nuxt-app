6785c98e53700e91db27a84f9f82b2152c0f8143
SHARON <sharushj690@gmail.com>
2019-02-11 14:56:03 +0530

    Updating Vuetify to latest & primary color as blue darken-2 (#182)

--- template/frameworks/vuetify/plugins/vuetify.js
+++ template/frameworks/vuetify/plugins/vuetify.js
@@ -4,7 +4,7 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
+    primary: colors.blue.darken-2,
     accent: colors.grey.darken3,
     secondary: colors.amber.darken3,
     info: colors.teal.lighten1,
