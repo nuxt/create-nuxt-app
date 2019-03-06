85a29ec62232035395ca94e9718205a21b2d8671
Dmitry Molotkov <aldarund@gmail.com>
2019-03-06 18:04:15 +0300

    fix: add coverage config to jest setup (#198)

--- template/frameworks/jest/jest.config.js
+++ template/frameworks/jest/jest.config.js
@@ -8,5 +8,10 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
-  }
+  },
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+      "<rootDir>/components/**/*.vue",
+      "<rootDir>/pages/**/*.vue"
+  ]
 }
