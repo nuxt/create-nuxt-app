1929ef4fa54867f2164d97886853786d5473566d
Takehiro Takahashi <taka.mc.0509@gmail.com>
2019-04-29 16:54:50 +0900

    add error page

---
+++ template/frameworks/vuetify/layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ this.pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ this.otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: ['error'],
+  head () {
+    const pageTitle = this.error.statusCode === 404
+      ? this.pageNotFound
+      : this.otherError
+    return {
+      pageTitle
+    }
+  },
+  data() {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred',
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
