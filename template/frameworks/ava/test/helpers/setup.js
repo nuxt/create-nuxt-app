b3677f2d0617af818ebbab947e3db3a5ba29773a
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-05-02 01:02:32 +0530

    Issue with jsdom - need this with ava for tests to run smoothly

--- template/frameworks/ava/test/helpers/setup.js
+++ template/frameworks/ava/test/helpers/setup.js
@@ -4,6 +4,9 @@ const Vue = require('vue')
 
 Vue.config.productionTip = false
 
+// https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
+window.Date = global.Date = Date;
+
 // Setup vue files to be processed by `require-extension-hooks-vue`
 hooks('vue')
   .plugin('vue')
