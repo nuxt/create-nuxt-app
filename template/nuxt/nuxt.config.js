9d236d75237facb24ccf3221c81f2982b3603077
pooya parsa <pooya@pi0.ir>
2019-05-14 01:20:05 +0430

    fallback to empty string

--- template/nuxt/nuxt.config.js
+++ template/nuxt/nuxt.config.js
@@ -29,7 +29,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }<% if (ui === 'vuetify') { %>,
