7d7810ca3e33f7745effbd780b6eca59f741024c
pooya parsa <pooya@pi0.ir>
2019-05-14 01:22:10 +0430

    npm_package_name

--- template/nuxt/nuxt.config.js
+++ template/nuxt/nuxt.config.js
@@ -25,7 +25,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
