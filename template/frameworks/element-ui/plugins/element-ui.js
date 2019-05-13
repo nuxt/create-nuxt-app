5d9251e108dea5fbaed97b3ffc49e8b91d96a296
pooya parsa <pooya@pi0.ir>
2019-05-14 01:09:51 +0430

    fix(element): register element UI only once

--- template/frameworks/element-ui/plugins/element-ui.js
+++ template/frameworks/element-ui/plugins/element-ui.js
@@ -2,6 +2,4 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 
-export default () => {
-  Vue.use(Element, { locale })
-}
+Vue.use(Element, { locale })
