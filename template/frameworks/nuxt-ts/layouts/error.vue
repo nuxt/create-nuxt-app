79bf7768a91a1db8f943e949ddc752629fdde839
Takehiro Takahashi <taka.mc.0509@gmail.com>
2019-06-03 16:29:50 +0900

    add nuxt error page

---
+++ template/frameworks/nuxt-ts/layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Error extends Vue {
+  layout: string = 'empty'
+  pageNotFound:string = '404 Not Found'
+  otherError:string = 'An error occurred'
+  @Prop({ type: Object, default: null }) error: Object
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
