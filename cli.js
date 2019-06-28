5e4ae345286cfaeffdc2c39581942816e6dcbd24
James George <jamesgeorge998001@gmail.com>
2019-06-28 17:21:52 +0530

    chore: update usage information (#271)

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ const generator = path.resolve(__dirname, './')
 const cli = cac('create-nuxt-app')
 
 cli
-  .command('[out-dir]', 'Generate in a custom directory or current directory')
+  .command('[out-dir] [options]', 'Generate in a custom directory or current directory')
   .option('--edge', 'To install `nuxt-edge` instead of `nuxt`')
   .action((outDir = '.') => {
     console.log(chalk`{cyan create-nuxt-app v${version}}`)
