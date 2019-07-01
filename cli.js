d2917a9d4ea987966048bdd0c7c268ceb165718f
James George <jamesgeorge998001@gmail.com>
2019-07-01 23:48:57 +0530

    feat(chore): included a dedicated option to show up envinfo (#272)

--- cli.js
+++ cli.js
@@ -3,16 +3,36 @@ const path = require('path')
 const sao = require('sao')
 const cac = require('cac')
 const chalk = require('chalk')
+const envinfo = require('envinfo')
 const { version } = require('./package.json')
 
 const generator = path.resolve(__dirname, './')
 
 const cli = cac('create-nuxt-app')
 
+const showEnvInfo = async () => {
+  console.log(chalk.bold('\nEnvironment Info:'))
+  const result = await envinfo
+    .run({
+      System: ['OS', 'CPU'],
+      Binaries: ['Node', 'Yarn', 'npm'],
+      Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+      npmGlobalPackages: ['nuxt', 'create-nuxt-app']
+    })
+  console.log(result)
+  process.exit(1)
+}
+
 cli
   .command('[out-dir] [options]', 'Generate in a custom directory or current directory')
   .option('--edge', 'To install `nuxt-edge` instead of `nuxt`')
+  .option('--info', 'Print out debugging information relating to the local environment')
   .action((outDir = '.') => {
+    const hasInfoArg = process.argv.slice(2)[0] === '--info'
+    if (hasInfoArg) {
+      return showEnvInfo()
+    }
+    console.log()
     console.log(chalk`{cyan create-nuxt-app v${version}}`)
     console.log(chalk`✨  Generating Nuxt.js project in {cyan ${outDir}}`)
 
