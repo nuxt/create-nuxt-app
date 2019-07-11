e34a1235d1fe0d8d1a7021d53159fc4ff2cf292d
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-07-12 00:55:28 +0530

    chore: add window.Date in `ava` setup

--- template/frameworks/ava/test/_setup.js
+++ template/frameworks/ava/test/_setup.js
@@ -4,5 +4,8 @@ const Vue = require('vue')
 
 Vue.config.productionTip = false
 
+// https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
+window.Date = global.Date = Date;
+
 hooks('vue').plugin('vue').push()
 hooks(['vue', 'js']).plugin('babel').push()
