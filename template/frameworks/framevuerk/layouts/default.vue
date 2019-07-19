4f77fd9c63d0f22597a584047fdcecfa68a709f2
Amir Momenian <nainemom@gmail.com>
2019-07-19 20:27:37 +0430

    feat: initial commit for framevuerk template

---
+++ template/frameworks/framevuerk/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <nuxt />
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 15px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+html,
+body,
+#__nuxt,
+#__layout {
+  min-height: 100%;
+  height: 100%;
+  position: initial;
+}
+body .material-icons {
+  vertical-align: middle;
+}
+</style>
---
+++ template/frameworks/framevuerk/plugins/framevuerk.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Framevuerk from 'framevuerk/dist/framevuerk-nuxt.min.js'
+
+export default () => {
+  Vue.use(Framevuerk)
+}
