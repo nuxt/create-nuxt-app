1e266c6331e4e3c30fce38d4981db6c969b53707
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-07-28 21:30:54 +0530

    fix: update way to import Vue Component in tests

--- template/frameworks/ava/test/specs/Logo.spec.js
+++ template/frameworks/ava/test/specs/Logo.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import test from 'ava'
-import Logo from '../../components/Logo.vue'
+import Logo from '@/components/Logo.vue'
 
 test('is a Vue instance', (t) => {
   const wrapper = mount(Logo)
