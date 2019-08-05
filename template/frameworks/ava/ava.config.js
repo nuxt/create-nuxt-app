4c26ca1314af511bd86c762768e8224f8cd739d1
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-08-05 13:10:20 +0530

    fix: add babel prop inside ava.config.js

--- template/frameworks/ava/ava.config.js
+++ template/frameworks/ava/ava.config.js
@@ -2,6 +2,22 @@ export default {
   require: ['./test/ava.setup.js'],
   sources: ['**/*.{js,vue}'],
   files: ['test/specs/**/*'],
+  babel: {
+    testOptions: {
+      plugins: [
+        [
+          'module-resolver',
+          {
+            root: ['.'],
+            alias: {
+              '@': '.',
+              '~': '.'
+            }
+          }
+        ]
+      ]
+    }
+  },
   tap: true,
   verbose: true
 }
