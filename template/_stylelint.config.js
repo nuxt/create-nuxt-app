4011b19143211001246c8dff512a202c34458f85
Tony <t@zeg.io>
2019-11-28 13:44:06 -0600

    Add `rules` to config
    The current implementation prevents `npm run dev` from building the client if stylelint was selected.  Adding this one property enables it to build properly out of the box.

--- template/_stylelint.config.js
+++ template/_stylelint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  rules: {
+    // add your custom config here
+    // https://stylelint.io/user-guide/configuration
+  }
 }
