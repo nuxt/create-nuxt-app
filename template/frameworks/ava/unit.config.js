a66685d88453ad311e32ab2e430baad47683b9f9
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-01-29 16:40:49 +0530

    chore: remove unnecessary config files

--- template/frameworks/ava/unit.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  require: ['./test/ava.setup.js'],
-  ignoredByWatcher: ['!**/*.{js,vue}'],
-  babel: true,
-  tap: true,
-  verbose: true,
-  color: true,
-  files: ['test/specs/**/*']
-}
