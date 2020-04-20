d6928d4b46046b144114782110b3f06deb3febb5
Clark Du <clark.duxin@gmail.com>
2020-04-20 23:26:09 +0100

    update test

--- packages/cna-template/template/frameworks/ava/test/helpers/ava.setup.js
+++ packages/cna-template/template/frameworks/ava/test/helpers/ava.setup.js
@@ -1,11 +1,15 @@
-require('browser-env')()
 const hooks = require('require-extension-hooks')
 const Vue = require('vue')
-
 Vue.config.productionTip = false
 
-// https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
-window.Date = global.Date = Date
+if (process.env.TEST === 'unit') {
+  require('browser-env')()
+  // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
+  window.Date = global.Date = Date
+}
 
 hooks('vue').plugin('vue').push()
-hooks(['vue', 'js']).exclude(({ filename }) => filename.match(/\/node_modules\//)).plugin('babel').push()
+hooks(['vue', 'js'])
+  .exclude(({ filename }) => filename.match(/\/node_modules\//))
+  .plugin('babel')
+  .push()
