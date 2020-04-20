20904fa4ce85a666928538ae48c0ceee3a47b56f
Clark Du <clark.duxin@gmail.com>
2020-04-20 23:38:51 +0100

    fix e2e test

--- packages/cna-template/template/frameworks/ava/test/helpers/ava.setup.js
+++ packages/cna-template/template/frameworks/ava/test/helpers/ava.setup.js
@@ -1,13 +1,13 @@
-const hooks = require('require-extension-hooks')
-const Vue = require('vue')
-Vue.config.productionTip = false
-
 if (process.env.TEST === 'unit') {
-  require('browser-env')()
+  require('jsdom-global')()
   // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
   window.Date = global.Date = Date
 }
 
+const Vue = require('vue')
+Vue.config.productionTip = false
+
+const hooks = require('require-extension-hooks')
 hooks('vue').plugin('vue').push()
 hooks(['vue', 'js'])
   .exclude(({ filename }) => filename.match(/\/node_modules\//))
