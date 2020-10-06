ed9e7e80830af6630d9250e5007ee3f681b52a26
pooya parsa <pyapar@gmail.com>
2020-10-06 12:28:34 +0200

    chore: lint only for push

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'yarn lint && yarn test -u',
+    'pre-commit': 'yarn test -u',
     'pre-push': 'yarn lint && yarn test'
   }
 }
