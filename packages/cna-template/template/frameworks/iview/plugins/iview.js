43da55582dc87bb1a1f80747b70181bc6f5fb174
Giuseppe Campanelli <suprememilanfan@gmail.com>
2021-02-12 14:09:10 -0500

    feat: update iview to viewui (#714)

--- packages/cna-template/template/frameworks/iview/plugins/iview.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import iView from 'iview'
-import locale from 'iview/dist/locale/en-US' // Change locale, check node_modules/iview/dist/locale
-
-Vue.use(iView, {
-  locale
-})
---
+++ packages/cna-template/template/frameworks/view-ui/plugins/view-ui.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import ViewUI from 'view-design'
+import locale from 'view-design/dist/locale/en-US' // Change locale, check node_modules/view-design/dist/locale
+
+Vue.use(ViewUI, {
+  locale
+})
