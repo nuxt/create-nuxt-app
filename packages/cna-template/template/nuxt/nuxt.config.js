2a78017593530ca17a6b9fdadaa53c3e8d2b5f42
scsc <scscgit@users.noreply.github.com>
2021-07-18 23:44:47 +0200

    fix(axios): avoid hard-coded base URL localhost:3000, fixes #641

--- packages/cna-template/template/nuxt/nuxt.config.js
+++ packages/cna-template/template/nuxt/nuxt.config.js
@@ -137,7 +137,10 @@
   <%_ if (axios) { _%>
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
   <%_ } _%>
   <%_ if (pwa) { _%>
 
