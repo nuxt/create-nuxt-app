3a55213e0757d0ce7f07dc6ba0c4eae0eecda94f
Harlan Wilton <harlan@harlanzw.com>
2021-11-18 23:33:39 +1100

    chore(windicss): upgrade templates for v2 (#876)

---
+++ packages/cna-template/template/frameworks/windicss/windi.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@windicss/plugin-utils'
+
+export default defineConfig({
+  /**
+   * Write windi classes in html attributes.
+   * @see https://windicss.org/features/attributify.html
+   */
+  attributify: true,
+})
