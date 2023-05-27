1059dd371b9bb82e5f9d463cf13beee4658b4641
Matanel Mac <mmgordon.matanel@gmail.com>
2023-05-27 18:14:43 +0300

    removed stylelint-config-prettier

--- packages/cna-template/template/_stylelint.config.js
+++ packages/cna-template/template/_stylelint.config.js
@@ -2,10 +2,7 @@ module.exports = {
   customSyntax: 'postcss-html',
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
-    <%_ if (prettier) { _%>
-    'stylelint-config-prettier'
-    <%_ } _%>
+    'stylelint-config-recommended-vue'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
